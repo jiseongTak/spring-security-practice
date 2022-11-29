3ac3569815731d78d4969410eaafdc391d89829e
jiseongTak <jeff3877@gmail.com>
2022-11-29 23:53:20 +0900

    view 추가 로그인/회원가입 기능 추가

---
+++ src/main/java/practice/security/user/SignController.java
@@ -0,0 +1,32 @@
+package practice.security.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 회원가입
+ */
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/signup")
+public class SignController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String signup() {
+        return "signup";
+    }
+
+    @PostMapping
+    public String signup(@ModelAttribute UserRegisterDto userDto) {
+        userService.signup(userDto.getUsername(), userDto.getPassword());
+
+        //로그인 페이지로 이동한다.
+        return "redirect:login";
+    }
+}
